79f4b9d26a507a66e91b465f388339ee31712021
James Deathe <james.deathe@jadu.net>
2015-11-27 12:20:07 +0000

    ISSUE 33: Relocate network helper functions out of config file and into the main run file.

--- run.sh
+++ run.sh
@@ -43,6 +43,123 @@ remove_docker_container_name ()
 	fi
 }
 
+get_docker_host_bridge_ip_addr ()
+{
+	local IP
+	local INTERFACE=${1:-docker0}
+
+	if [[ ${DOCKER_HOST} != "" ]]; then
+		IP=$(echo ${DOCKER_HOST} | grep -oE "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}")
+
+		if [[ ${INTERFACE} == "docker0" ]] && [[ ${IP} != "127.0.0.1" ]]; then
+			# Assume 172.17.0.1/16 for remote docker hosts or VMs
+			IP=172.17.0.1/16
+		elif  [[ ${INTERFACE} == "eth1" ]] && [[ ${IP} != "127.0.0.1" ]]; then
+			# Assume a CIDR of /24 for remote docker hosts or VMs
+			IP=${IP}/24
+		fi
+	elif type "ip" &> /dev/null; then
+		IP=$(ip addr show ${INTERFACE} | grep -oE "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/[0-9]{1,2}")
+	fi
+
+	echo ${IP}
+}
+
+get_docker_host_bridge_ip ()
+{
+	local IP=$(get_docker_host_bridge_ip_addr ${1:-docker0})
+	local IP_PARTS=(${IP//\// })
+
+	echo ${IP_PARTS[0]}
+}
+
+get_docker_host_bridge_cidr ()
+{
+	local IP=$(get_docker_host_bridge_ip_addr ${1:-docker0})
+	local IP_PARTS=(${IP//\// })
+
+	echo ${IP_PARTS[1]}
+}
+
+get_docker_host_mysql_subnet ()
+{
+
+	local IP=$(get_docker_host_bridge_ip ${1:-docker0})
+	local CIDR=$(get_docker_host_bridge_cidr ${1:-docker0})
+	local IP_OCTETS=(${IP//./ })
+
+	# MySQL can only limit connections based on 8, 16, 24 or 32 bits
+	case ${CIDR} in
+	0)
+		# Any IP address/subnet
+		echo 0.0.0.0/0.0.0.0
+		;;
+	1|2|3|4|5|6|7|8)
+		# Class A - 10.0.0.0
+		echo ${IP_OCTETS[0]}.0.0.0/255.0.0.0
+		;;
+	9|10|11|12|13|14|15|16)
+		# Class B - 10.10.0.0
+		echo ${IP_OCTETS[0]}.${IP_OCTETS[1]}.0.0/255.255.0.0
+		;;
+	17|18|19|20|21|22|23|24)
+		# Class C - 10.10.10.0
+		echo ${IP_OCTETS[0]}.${IP_OCTETS[1]}.${IP_OCTETS[2]}.0/255.255.255.0
+		;;
+	*)
+		if [ "${IP}" == "" ] || [ "${IP}" == "0.0.0.0" ]; then
+			# Could not determin IP address or CIDR
+			echo 0.0.0.0/0.0.0.0
+		else
+			# Exact match on IP address only
+			echo ${IP}/255.255.255.255
+		fi
+		;;
+	esac
+}
+
+get_docker_container_ip ()
+{
+	echo $(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${1:-})
+}
+
+get_docker_container_cidr ()
+{
+	echo $(docker inspect --format '{{ .NetworkSettings.IPPrefixLen }}' ${1:-})
+}
+
+get_docker_container_mysql_subnet ()
+{
+
+	local IP=$(get_docker_container_ip ${1:-})
+	local CIDR=$(get_docker_container_cidr ${1:-})
+	local IP_OCTETS=(${IP//./ })
+
+	# MySQL can only limit connections based on 8, 16, 24 or 32 bits
+	case ${CIDR} in
+	0)
+		# Any IP address/subnet
+		echo 0.0.0.0/0.0.0.0
+		;;
+	1|2|3|4|5|6|7|8)
+		# Class A - 10.0.0.0
+		echo ${IP_OCTETS[0]}.0.0.0/255.0.0.0
+		;;
+	9|10|11|12|13|14|15|16)
+		# Class B - 10.10.0.0
+		echo ${IP_OCTETS[0]}.${IP_OCTETS[1]}.0.0/255.255.0.0
+		;;
+	17|18|19|20|21|22|23|24)
+		# Class C - 10.10.10.0
+		echo ${IP_OCTETS[0]}.${IP_OCTETS[1]}.${IP_OCTETS[2]}.0/255.255.255.0
+		;;
+	*)
+		# Exact Match IP - 10.10.10.0
+		echo ${IP}/255.255.255.255
+		;;
+	esac
+}
+
 # Configuration volume
 if ! have_docker_container_name ${VOLUME_CONFIG_NAME} ; then
 	# For configuration that is specific to the running container
@@ -114,6 +231,8 @@ else
 	DOCKER_PORT_OPTIONS="-p 3306:3306"
 fi
 
+MYSQL_SUBNET=${MYSQL_SUBNET:-$(get_docker_host_mysql_subnet docker0)}
+
 # In a sub-shell set xtrace - prints the docker command to screen for reference
 (
 set -x
@@ -121,7 +240,7 @@ docker run \
 	${DOCKER_OPERATOR_OPTIONS} \
 	--name ${DOCKER_NAME} \
 	${DOCKER_PORT_OPTIONS} \
-	--env MYSQL_SUBNET=${MYSQL_SUBNET:-%} \
+	--env MYSQL_SUBNET=${MYSQL_SUBNET} \
 	--volumes-from ${VOLUME_CONFIG_NAME} \
 	-v ${MOUNT_PATH_DATA}/${SERVICE_UNIT_NAME}/${SERVICE_UNIT_SHARED_GROUP}:/var/lib/mysql \
 	${DOCKER_IMAGE_REPOSITORY_NAME} -c "${DOCKER_COMMAND}"
