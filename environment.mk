c770d0cac4a13aa9ef15141d5084dbb0d52b03cf
James Deathe <james.deathe@gmail.com>
2016-05-29 00:44:33 +0100

    ISSUE 73: Don't use a predefined port mapping to port 22.

--- environment.mk
+++ environment.mk
@@ -8,7 +8,7 @@ DOCKER_USER := jdeathe
 DOCKER_IMAGE_NAME := centos-ssh-mysql
 DOCKER_IMAGE_TAG ?= latest
 DOCKER_NAME ?= mysql.pool-1.1.1
-DOCKER_PORT_MAP_TCP_22 ?= 2400
+DOCKER_PORT_MAP_TCP_22 ?= 
 DOCKER_PORT_MAP_TCP_3306 ?= 3306
 DOCKER_RESTART_POLICY ?= always # {no,on-failure[:max-retries],always,unless-stopped}
 
@@ -36,7 +36,6 @@ VOLUME_CONFIG_NAMED := false
 # ------------------------------------------------------------------------------
 # Application container configuration
 # ------------------------------------------------------------------------------
-# Upstream environment variables
 SSH_AUTHORIZED_KEYS ?=
 SSH_AUTOSTART_SSHD ?= false
 SSH_AUTOSTART_SSHD_BOOTSTRAP ?= false
@@ -51,7 +50,6 @@ SSH_USER_PASSWORD_HASHED ?= false
 SSH_USER_SHELL ?= /bin/bash
 SSH_USER_ID ?= 500:500
 # ------------------------------------------------------------------------------
-# Local environment variables
 MYSQL_ROOT_PASSWORD ?=
 MYSQL_SUBNET ?= 127.0.0.1
 MYSQL_USER ?=
