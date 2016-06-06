83e1f1d0e4f6eab14fbf11c3d2985a762fd5a464
James Deathe <james.deathe@gmail.com>
2016-06-06 13:20:09 +0100

    Set the target database for structure SQL template.

--- srv/mysql/init-data/data.sql
+++ srv/mysql/init-data/data.sql
@@ -1,3 +1,5 @@
 
--- Table data insert statements
+-- Set target database
 USE `%MYSQL_USER_DATABASE%`;
+
+-- Table data insert statements
--- srv/mysql/init-structure/structure.sql
+++ srv/mysql/init-structure/structure.sql
@@ -1,3 +1,8 @@
 
 -- Create user database
 CREATE DATABASE IF NOT EXISTS `%MYSQL_USER_DATABASE%`;
+
+-- Set target database
+USE `%MYSQL_USER_DATABASE%`;
+
+-- Table/View structure
